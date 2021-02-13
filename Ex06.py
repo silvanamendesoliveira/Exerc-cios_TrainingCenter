bd160c7081f04d478edd0dadcf99c8268a6c0be5
silvanamendesoliveira <77698082+silvanamendesoliveira@users.noreply.github.com>
2021-02-13 17:54:16 +0000

    Add files via upload

---
+++ Ex06.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+
+def soma_multiplos_abaixo_de(limite):
+    return sum([i for i in range(limite) if i%3 == 0 and i%5 == 0])
+
+print( "A soma de todos os múltiplos de 3 e 5 abaixo de 1000 é:", soma_multiplos_abaixo_de(1000))
